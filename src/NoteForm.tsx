939da000b6a4e4dcdea869ac18fcdb7135f16619
khazarglzd <khazarguluzade@gmail.com>
2024-05-17 15:36:18 +0400

    creating NoteForm component

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,7 +1,42 @@
+import { Form, Stack, Row, Col, Button } from "react-bootstrap";
+import CreatableReactSelect from "react-select/creatable";
+import { Link } from "react-router-dom";
+
 export function NoteForm() {
   return (
-    <>
-      <h1 className="mb-4">New Note</h1>
-    </>
+    <Form>
+      <Stack gap={4}>
+        <Row>
+          <Col>
+            <Form.Group controlId="title">
+              <Form.Label>Title</Form.Label>
+              <Form.Control required />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect isMulti className="select" />
+            </Form.Group>
+          </Col>
+        </Row>
+        <Form.Group controlId="markdown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Link to="">
+            <Button type="submit" variant="primary">
+              Save
+            </Button>
+          </Link>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
+      </Stack>
+    </Form>
   );
 }
