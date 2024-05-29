2d307bde695fdbeb02cb7bba62ca267a50dec4a0
khazarglzd <khazarguluzade@gmail.com>
2024-05-29 15:15:47 +0400

    styling Note page

--- src/Note.tsx
+++ src/Note.tsx
@@ -1,5 +1,7 @@
-import { Badge, Col, Row, Stack } from "react-bootstrap";
+import { Badge, Button, Col, Row, Stack } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { useNote } from "./NoteLayout";
+import ReactMarkdown from "react-markdown";
 
 export function Note() {
   const note = useNote();
@@ -18,7 +20,19 @@ export function Note() {
             </Stack>
           )}
         </Col>
+        <Col xs="auto">
+          <Stack gap={2} direction="horizontal">
+            <Link to={`/${note.id}/edit`}>
+              <Button variant="primary">Edit</Button>
+            </Link>
+            <Button variant="outline-danger">Delete</Button>
+            <Link to="/">
+              <Button variant="outline-secondary">Back</Button>
+            </Link>
+          </Stack>
+        </Col>
       </Row>
+      <ReactMarkdown>{note.markdown}</ReactMarkdown>
     </>
   );
 }
