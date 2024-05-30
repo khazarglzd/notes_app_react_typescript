86f6bd6c22630eb59851e15fa1658110106a479a
khazarglzd <khazarguluzade@gmail.com>
2024-05-31 00:48:57 +0400

    adding delete functionality for Note

--- src/App.tsx
+++ src/App.tsx
@@ -57,6 +57,12 @@ function App() {
     });
   }
 
+  function onDeleteNote(id: string) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((note) => note.id !== id);
+    });
+  }
+
   function onUpdateNote(id: string, { tags, ...data }: NoteData) {
     setNotes((prevNotes) => {
       return prevNotes.map((note) => {
@@ -91,7 +97,7 @@ function App() {
           }
         />
         <Route path="/:id" element={<NoteLayout notes={notesWithTags} />}>
-          <Route index element={<Note />} />
+          <Route index element={<Note onDelete={onDeleteNote} />} />
           <Route
             path="edit"
             element={
